2dcb6d983e5e63a85fb03f7425dd1ef9587bbbac
むずでょ <muzudho1@gmail.com>
2021-02-15 21:59:23 +0900

    Rename

---
+++ controller/nngs-engine-controller.go
@@ -0,0 +1,7 @@
+package controller
+
+// NngsEngineController - NNGS からの受信メッセージをさばきます。
+type NngsEngineController struct {
+	// EntryConf - 参加設定
+	EntryConf EntryConf
+}
--- controller/nngs-human-controller.go
+++
@@ -1,7 +0,0 @@
-package controller
-
-// NngsHumanController - NNGS からの受信メッセージをさばきます。
-type NngsHumanController struct {
-	// EntryConf - 参加設定
-	EntryConf EntryConf
-}
