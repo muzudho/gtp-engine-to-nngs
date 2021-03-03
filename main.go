0af4740be30991f70e28dcddfca70add32f272e3
むずでょ <muzudho1@gmail.com>
2021-03-04 00:31:45 +0900

    Update main.go

--- main.go
+++ main.go
@@ -19,36 +19,35 @@ import (
 
 func main() {
 	// Working directory
-	wdir, err := os.Getwd()
+	dwd, err := os.Getwd()
 	if err != nil {
 		// ここでは、ログはまだ設定できてない
-		panic(fmt.Sprintf("...GE2NNGS... wdir=%s", wdir))
+		panic(fmt.Sprintf("...GE2NNGS... DefaultWorkingDirectory=%s", dwd))
 	}
-	fmt.Printf("...GE2NNGS... wdir=%s\n", wdir)
+	fmt.Printf("...GE2NNGS... DefaultWorkingDirectory=%s\n", dwd)
 
-	// コマンドライン引数
-	workdir := flag.String("workdir", wdir, "Working directory path.")
+	// コマンドライン引数登録
+	wd := flag.String("workdir", dwd, "Working directory path.")
+	// 解析
 	flag.Parse()
-	fmt.Printf("...GE2NNGS... flag.Args()=%s\n", flag.Args())
-	fmt.Printf("...GE2NNGS... workdir=%s\n", *workdir)
-	connectorConfPath := filepath.Join(*workdir, "input/connector.conf.toml")
-	engineConfPath := filepath.Join(*workdir, "input/engine.conf.toml")
-	fmt.Printf("...GE2NNGS... connectorConfPath=%s\n", connectorConfPath)
-	fmt.Printf("...GE2NNGS... engineConfPath=%s\n", engineConfPath)
+
+	connectorConfPath := filepath.Join(*wd, "input/connector.conf.toml")
+	engineConfPath := filepath.Join(*wd, "input/engine.conf.toml")
+	fmt.Printf("...GE2NNGS... WorkingDirectory=[%s] ConnectorConf=[%s] EngineConf=[%s]\n", *wd, connectorConfPath, engineConfPath)
 
 	// グローバル変数の作成
 	g.G = *new(g.GlobalVariables)
 
 	// ロガーの作成。
 	g.G.Log = *kwu.NewLogger(
-		filepath.Join(*workdir, "output/connector/trace.log"),
-		filepath.Join(*workdir, "output/connector/debug.log"),
-		filepath.Join(*workdir, "output/connector/info.log"),
-		filepath.Join(*workdir, "output/connector/notice.log"),
-		filepath.Join(*workdir, "output/connector/warn.log"),
-		filepath.Join(*workdir, "output/connector/error.log"),
-		filepath.Join(*workdir, "output/connector/fatal.log"),
-		filepath.Join(*workdir, "output/connector/print.log"))
+		filepath.Join(*wd, "output/connector/trace.log"),
+		filepath.Join(*wd, "output/connector/debug.log"),
+		filepath.Join(*wd, "output/connector/info.log"),
+		filepath.Join(*wd, "output/connector/notice.log"),
+		filepath.Join(*wd, "output/connector/warn.log"),
+		filepath.Join(*wd, "output/connector/error.log"),
+		filepath.Join(*wd, "output/connector/fatal.log"),
+		filepath.Join(*wd, "output/connector/print.log"))
 
 	// 既存のログ・ファイルを削除。
 	g.G.Log.RemoveAllOldLogs()
@@ -71,25 +70,26 @@ func main() {
 	// 設定ファイル読込
 	engineConf, err := kwui.LoadEngineConf(engineConfPath)
 	if err != nil {
-		panic(g.G.Chat.Fatal("...GE2NNGS... engineConfPath=[%s] err=[%s]\n", engineConfPath, err))
+		panic(g.G.Chat.Fatal("...GE2NNGS... engineConf err=[%s]\n", err))
 	}
 
 	connectorConf, err := ui.LoadConnectorConf(connectorConfPath)
 	if err != nil {
-		panic(g.G.Chat.Fatal("...GE2NNGS... connectorConfPath=[%s] err=[%s]\n", connectorConfPath, err))
+		panic(g.G.Chat.Fatal("...GE2NNGS... connectorConf err=[%s]\n", err))
 	}
 
 	g.G.Chat.Trace("...GE2NNGS... (^q^) プレイヤーのタイプ☆ [%s]\n", connectorConf.User.InterfaceType)
 
 	// 思考エンジンを起動
-	startEngine(engineConf, connectorConf, workdir)
+	startEngine(engineConf, connectorConf, wd)
 
 	g.G.Chat.Trace("...GE2NNGS... End program\n")
 }
 
 // 思考エンジンを起動
-func startEngine(engineConf *kwe.EngineConf, connectorConf *e.ConnectorConf, workdir *string) {
-	parameters := strings.Split("--workdir "+*workdir+" "+connectorConf.User.EngineCommandOption, " ")
+// `wd` - Working directory
+func startEngine(engineConf *kwe.EngineConf, connectorConf *e.ConnectorConf, wd *string) {
+	parameters := strings.Split("--workdir "+*wd+" "+connectorConf.User.EngineCommandOption, " ")
 	parametersString := strings.Join(parameters, " ")
 	parametersString = strings.TrimRight(parametersString, " ")
 	g.G.Chat.Trace("...GE2NNGS... (^q^) GTP対応の思考エンジンを起動するぜ☆ 途中で終わりたかったら [Ctrl]+[C]\n")
