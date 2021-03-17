9de98e1d3974583d54fc433d0ed75e65c96b9c7d
むずでょ <muzudho1@gmail.com>
2021-03-17 19:54:21 +0900

    CLI

---
+++ cli/log-in-black.ps1
@@ -0,0 +1,5 @@
+﻿# UTF-8 with BOM
+# ファイルパスは書きかえてください
+
+# 黒番の例
+Start-Process "gtp-engine-to-nngs.exe" -ArgumentList "--workdir C:/Users/むずでょ/go/src/github.com/muzudho/gtp-engine-to-nngs/workspace-b" -NoNewWindow -Wait
---
+++ cli/log-in-white.ps1
@@ -0,0 +1,5 @@
+﻿# UTF-8 with BOM
+# ファイルパスは書きかえてください
+
+# 白番の例
+Start-Process -FilePath "gtp-engine-to-nngs.exe" -ArgumentList "--workdir C:/Users/むずでょ/go/src/github.com/muzudho/gtp-engine-to-nngs/workspace-w" -NoNewWindow -Wait
